444b76f146e45b36a7274b04b11d0e2e651b9a17
Mito Kehayov <Mitokehayov@gmail.com>
2019-01-27 14:36:04 +0100

    init

---
+++ Adal.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Adal.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
