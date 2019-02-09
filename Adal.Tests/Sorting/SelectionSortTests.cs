7393544a448639105f07d9a6d61e8722df40be23
Mito Kehayov <Mitokehayov@gmail.com>
2019-02-09 20:10:52 +0100

    implemented selection sort

---
+++ Adal.Tests/Sorting/SelectionSortTests.cs
@@ -0,0 +1,31 @@
+﻿namespace Adal.Tests.Sorting
+{
+    using Adal.Sorting;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+
+    [TestClass]
+    public class SelectionSortTests
+    {
+        private readonly int[] arrayToBeSorted = new int[] { 3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48 };
+        private readonly int[] sortedArray = new int[] { 2, 3, 4, 5, 15, 19, 26, 27, 36, 38, 44, 46, 47, 48, 50 };
+
+        [TestMethod]
+        public void TestIfArrayCorrectlySorted()
+        {
+            var result = Sorting<int>.SelectionSort(arrayToBeSorted);
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Assert.AreEqual(sortedArray[i], result[i]);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestIfReturnedExceptionWhenInputNull()
+        {
+            var result = Sorting<int>.SelectionSort(null);
+        }
+    }
+}
---
+++ Adal/Sorting/Sorting.cs
@@ -0,0 +1,47 @@
+﻿namespace Adal.Sorting
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class Sorting<T> where T : IComparable
+    {
+        public static T[] SelectionSort(ICollection<T> data)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException("Parameter " + nameof(data) + ", cannot be null!");
+            }
+        
+            T[] sortedData = new T[data.Count];
+            data.CopyTo(sortedData, 0);
+
+            int minValueElementIndex;
+            bool shouldSwap = false;
+
+            for (int i = 0; i < sortedData.Length; i++)
+            {
+                minValueElementIndex = i;
+
+                for (int j = i + 1; j < sortedData.Length; j++)
+                {
+                    if (sortedData[minValueElementIndex].CompareTo(sortedData[j]) > 0)
+                    {
+                        minValueElementIndex = j;
+                        shouldSwap = true;
+                    }
+                }
+
+                if (shouldSwap)
+                {
+                    var tmp = sortedData[i];
+                    sortedData[i] = sortedData[minValueElementIndex];
+                    sortedData[minValueElementIndex] = tmp;
+
+                    shouldSwap = false;
+                }
+            }
+
+            return sortedData;
+        }
+    }
+}
